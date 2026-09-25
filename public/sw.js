6dbfc101073dd9932fe066f746042991dcf22051
Bill bharat.co.in <117995755+Prateek004@users.noreply.github.com>
2026-09-25 14:01:40 +0530

    Bump CACHE_VERSION to v4.5

--- public/sw.js
+++ public/sw.js
@@ -7,7 +7,7 @@
  * IMPORTANT: Bump CACHE_VERSION on every deploy that changes static files.
  */
 
-const CACHE_VERSION   = 'dormbook-v4.4';
+const CACHE_VERSION   = 'dormbook-v4.5';
 const STATIC_CACHE    = `${CACHE_VERSION}-static`;
 
 const STATIC_ASSETS = [
