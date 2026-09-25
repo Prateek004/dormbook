332ecd496dd28767b949226cc8c6bcada6f75388
Prateek Mukund <117995755+Prateek004@users.noreply.github.com>
2026-09-26 02:39:01 +0530

    Update CACHE_VERSION to v4.6

--- public/sw.js
+++ public/sw.js
@@ -7,7 +7,7 @@
  * IMPORTANT: Bump CACHE_VERSION on every deploy that changes static files.
  */
 
-const CACHE_VERSION   = 'dormbook-v4.5';
+const CACHE_VERSION   = 'dormbook-v4.6';
 const STATIC_CACHE    = `${CACHE_VERSION}-static`;
 
 const STATIC_ASSETS = [
